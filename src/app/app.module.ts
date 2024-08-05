4e6ab7003457e9094e5d258fc2e81f542560427b
raghanharkesh <raghan007@gmail.com>
2024-08-06 01:31:27 +0530

    working cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { DemoproductsComponent } from './demoproducts/demoproducts.component';
-import { FooterComponent } from './footer/footer.component';
+// import { HeaderComponent } from './header/header.component';
+
+// import { FooterComponent } from './footer/footer.component';
 import { LoginpageComponent } from './loginpage/loginpage.component';
-import { SelectionsComponent } from './selections/selections.component';
+
 import { AboutsectionComponent } from './aboutsection/aboutsection.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ContactComponent } from './contact/contact.component';
 import { CartComponent } from './cart/cart.component';
-import { ProductlistingComponent } from './productlisting/productlisting.component';
+import { ProductListingComponent } from './productlisting/productlisting.component';
+import { CartService } from './services/cart.service';
+import { RouterModule, Routes } from '@angular/router';
+import { MiddlesectionComponent } from './middlesection/middlesection.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  { path: '', component: MiddlesectionComponent },
+  { path: 'loginpage', component: LoginpageComponent },
+  { path: 'aboutsection', component: AboutsectionComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'productlisting', component: ProductListingComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DemoproductsComponent,
-    FooterComponent,
     LoginpageComponent,
-    SelectionsComponent,
+    FooterComponent,
     AboutsectionComponent,
     GalleryComponent,
     ContactComponent,
     CartComponent,
-    ProductlistingComponent
+    ProductListingComponent,
+    MiddlesectionComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
+  providers: [CartService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
