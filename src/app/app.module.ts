1ebe4dc93482247a9bc2b735f99380e9513689c5
Banik <sanbanik@deloitte.com>
2024-08-05 20:59:15 +0530

    routing commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,58 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { DemoproductsComponent } from './demoproducts/demoproducts.component';
-import { FooterComponent } from './footer/footer.component';
+// import { HeaderComponent } from './header/header.component';
+
+// import { FooterComponent } from './footer/footer.component';
 import { LoginpageComponent } from './loginpage/loginpage.component';
-import { SelectionsComponent } from './selections/selections.component';
+
 import { AboutsectionComponent } from './aboutsection/aboutsection.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ContactComponent } from './contact/contact.component';
 import { CartComponent } from './cart/cart.component';
 import { ProductlistingComponent } from './productlisting/productlisting.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { MiddlesectionComponent } from './middlesection/middlesection.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  { path: '', component: MiddlesectionComponent},
+  
+  { path: 'loginpage', component: LoginpageComponent},
+  
+  { path: 'aboutsection', component: AboutsectionComponent},
+  { path: 'gallery', component: GalleryComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'cart', component: CartComponent},
+  { path: 'productlisting', component: ProductlistingComponent}
+  
+]
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DemoproductsComponent,
-    FooterComponent,
     LoginpageComponent,
-    SelectionsComponent,
+    FooterComponent,
     AboutsectionComponent,
     GalleryComponent,
     ContactComponent,
     CartComponent,
-    ProductlistingComponent
+    ProductlistingComponent,
+    MiddlesectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
